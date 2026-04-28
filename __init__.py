78f64f0e0b1f99d0016fa0f9edea48b8b4f06f14
stom <tomsteventon@gmail.com>
2026-04-29 00:25:15 +0100

    chore: release 0.3.1

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
 	"name"       : "Asset Exporter",
 	"description": "Quickly export multiple Collections to glTF or FBX",
 	"author"     : "Tom Steventon - stom66",
-	"version"    : (0, 3, 0),
+	"version"    : (0, 3, 1),
 	"blender"    : (4, 1, 0),
 	"location"   : "3D Viewport -> Sidebar -> Asset Exporter",
 	"category"   : "Import-Export",
